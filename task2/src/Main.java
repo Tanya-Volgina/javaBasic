4c59abebe693f0e94292582bf126beaadf621208
Tanya-Volgina <tanyavolgina13@gmail.com>
2022-09-11 14:09:47 +0300

    Task №2

---
+++ task2/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    private static void task1() {
+        double a = 3.192;
+        System.out.println(a);
+    }
+
+    private static void task2() {
+        byte b = 127;
+        System.out.println(b);
+    }
+}
