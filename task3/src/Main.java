8cc0cf7619f9d36e8ca8446554b764d3e3115d27
Tanya-Volgina <tanyavolgina13@gmail.com>
2022-09-11 21:22:21 +0300

    Task №3

--- task3/src/Main.java
+++ task3/src/Main.java
@@ -1,18 +1,16 @@
-public class task3 {
+public class Main {
     public static void main(String[] args) {
         int[] array = {-3, 205, 111, 2, 100};
 
         int temp = array[0];
-        array[0] = array[array.length-1];
-        array[array.length-1] = temp;
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
 
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
-            sum = array[0] + array[array.length/2];
+            sum = array[0] + array[array.length / 2];
         }
 
         System.out.println(sum);
     }
 }
-
-
